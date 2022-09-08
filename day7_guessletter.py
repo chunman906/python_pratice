97fe2b95dbee4af17f6c6c45980b5fe7125bfab8
chun man poon <chunman906@hotmail.com>
2022-09-09 00:22:35 +0100

    final version

--- day7_guessletter.py
+++ day7_guessletter.py
@@ -1,7 +1,17 @@
 import random
-word_list = ["aardvark", "baboon", "camel"]
+word_list = [
+'abruptly',
+'absurd',
+'abyss',
+'affix',
+'askew',
+'avenue',]
 chosen_word = random.choice(word_list)
 word_length = len(chosen_word)
+
+end_of_game = False
+lives = 6
+#TODO-3: - Import the logo from hangman_art.py and print it at the start of the game.
 
 #Testing code
 print(f'Pssst, the solution is {chosen_word}.')
@@ -13,19 +23,33 @@ for _ in range(word_length):
 
 #TODO-1: - Use a while loop to let the user guess again. The loop should only stop once the user has guessed all the letters in the chosen_word and 'display' has no more blanks ("_"). Then you can tell the user they've won.
 
-def check():
-  for word in display:
-    if word == "_":
-        return True
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
 
-#Check guessed letter
-while check() == True:
-  guess = input("Guess a letter: ").lower()
-  for position in range(word_length):
-      letter = chosen_word[position]
-      print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
-      if letter == guess:
-          display[position] = letter
-  print(display)
-else:
-  print("You won!")
+    #TODO-4: - If the user has entered a letter they've already guessed, print the letter and let them know.
+
+    #Check guessed letter
+    for position in range(word_length):
+        letter = chosen_word[position]
+        print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
+        if letter == guess:
+            display[position] = letter
+        elif guess in display:
+            print(f"You've entered {guess}, it has been already guessed!")
+
+    #Check if user is wrong.
+    if guess not in chosen_word:
+        print(f"You've entered {guess}.It is not in the word!")
+        #TODO-5: - If the letter is not in the chosen_word, print out the letter and let them know it's not in the word.
+        lives -= 1
+        if lives == 0:
+            end_of_game = True
+            print("You lose.")
+
+    #Join all the elements in the list and turn it into a String.
+    print(f"{' '.join(display)}")
+
+    #Check if user has got all letters.
+    if "_" not in display:
+        end_of_game = True
+        print("You win.")
