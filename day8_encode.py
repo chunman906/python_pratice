9a67598ada1b62016230e6a86bb475f96afc170d
chun man poon <chunman906@hotmail.com>
2022-09-11 00:04:51 +0100

    finalise game

--- day8_encode.py
+++ day8_encode.py
@@ -1,21 +1,32 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-
-def caesar(start_text, shift_amount, choice):
-    end_text = ""
-    if choice == "decode":
-      shift_amount *= -1
-    for letter in start_text:
-      position = alphabet.index(letter)
+def caesar(start_text, shift_amount, cipher_direction):
+  end_text = ""
+  if cipher_direction == "decode":
+    shift_amount *= -1
+  for char in start_text:
+    if char not in alphabet:
+      end_text += char
+    else:
+      position = alphabet.index(char)
       new_position = position + shift_amount
+      if new_position >= 52:
+        new_position %= 52
       end_text += alphabet[new_position]
-    print(f"The {choice}d text is {end_text}")
 
-caesar(start_text=text, shift_amount=shift, choice=direction)
+  print(f"Here's the {cipher_direction}d result: {end_text}")
 
+continue_game = True
+
+while continue_game:
+  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+  text = input("Type your message:\n").lower()
+  shift = int(input("Type the shift number:\n"))
+
+  caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+  choice = input("Do you want to restart the game? Type 'yes' to restart, type 'no' to quit. \n")
+  if choice == 'no':
+    continue_game = False
 #Longer version:
 # def encrypt(plain_text, shift_amount):
 #   cipher_text = ""
